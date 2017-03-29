8dc8bf9372dec2dd721f5bd73e570a72ee8a604e
Hyun Kim <hkim@live.com.au>
2017-03-29 20:58:33 +1100

    Question branc

--- db/migrate/20170329091424_create_answers.rb
+++
@@ -1,9 +0,0 @@
-class CreateAnswers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :answers do |t|
-      t.text :content
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329092934) do
+ActiveRecord::Schema.define(version: 20170329095653) do
 
   create_table "answers", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
